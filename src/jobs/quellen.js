fb74122ea7d87a11e336650537d5478962135ed7
freddykrueger88 <117486731+freddykrueger88@users.noreply.github.com>
2026-05-10 04:08:03 +0200

    fix: src/jobs/quellen.js stub anlegen (MODULE_NOT_FOUND)

---
+++ src/jobs/quellen.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Job-Portal-Adapter.
+ * Jeder Eintrag muss { id, fetch({ suchbegriff, ort, umkreis }) } exportieren.
+ * fetch() gibt ein Promise<Array> zurück.
+ *
+ * TODO: echte Adapter für Stepstone, Indeed, etc. hier eintragen.
+ */
+
+module.exports = [];
